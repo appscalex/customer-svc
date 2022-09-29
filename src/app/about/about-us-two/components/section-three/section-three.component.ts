c4963321cf7ac9519aa93b78b72152c70641dee9
Debajit Saikia <dsaikia@compensationcloud.com>
2022-09-29 01:04:17 -0700

    chanegs

--- src/app/about/about-us-two/components/section-three/section-three.component.ts
+++ src/app/about/about-us-two/components/section-three/section-three.component.ts
@@ -8,40 +8,40 @@ export class SectionThreeComponent implements OnInit {
 
   List: any[] = [
     {
-      title: 'Devops',
+      title: 'Deployment',
       image: './assets/images/services/01.png',
-      desc: 'The most powerful tool we have as developers is automation.',
-      checkList: ['Advance Advisory Team', 'Professional Consulting Services', '24/7 Support Help Center']
+      desc: 'It is a DevOps tool for monitoring execution of repeated tasks.',
+      checkList: ['It increases the scale of automation.', 'It offers to support building and testing virtually any project.', 'It can easily set up and configured via web interface.']
     },
     {
-      title: 'Analytics',
+      title: 'Visualization',
       image: './assets/images/services/02.png',
-      desc: 'The most powerful tool we have as developers is automation.',
-      checkList: ['Advance Advisory Team', 'Professional Consulting Services', '24/7 Support Help Center']
+      desc: 'It is an Integrated Visual platform for DevOps engineers.',
+      checkList: ['Allows users planning the automation process.', 'Each execution is automatically documented.', 'User can trigger an execution of a map from remote events.']
     },
     {
-      title: 'Reporting',
+      title: 'Monitoring',
       image: './assets/images/services/03.png',
-      desc: 'The most powerful tool we have as developers is automation.',
-      checkList: ['Advance Advisory Team', 'Professional Consulting Services', '24/7 Support Help Center']
+      desc: 'It is a DevOps platform for high-speed server monitoring management.',
+      checkList: ['Start monitoring and collecting data for all environments.', 'Centralized log management and server monitoring.', 'Watch all the new events arrive in near real-time.']
     },
     {
-      title: 'Costing',
+      title: 'Auditing',
       image: './assets/images/services/04.png',
-      desc: 'The most powerful tool we have as developers is automation.',
-      checkList: ['Advance Advisory Team', 'Professional Consulting Services', '24/7 Support Help Center']
+      desc: 'It is a DevOps solution for continuous configuration and auditing.',
+      checkList: ['Workflow offers various options like non-expert users and expert users.', 'Enforce configuration over time.', 'Provide Inventory of all managed nodes.']
     },
     {
-      title: 'Intelligence',
+      title: 'Testing',
       image: './assets/images/services/06.png',
-      desc: 'The most powerful tool we have as developers is automation.',
-      checkList: ['Advance Advisory Team', 'Professional Consulting Services', '24/7 Support Help Center']
+      desc: 'It offers the only DevOps and test automation platform engineered for all.',
+      checkList: ['Shift quality left highlight risk, impact and issues.', 'Fully customizable approval process.', 'Full audit trail enables regulatory compliance.']
     },
     {
-      title: 'Data Visualization',
+      title: 'Delivery',
       image: './assets/images/services/05.png',
-      desc: 'The most powerful tool we have as developers is automation.',
-      checkList: ['Advance Advisory Team', 'Professional Consulting Services', '24/7 Support Help Center']
+      desc: 'It is a DevOps tool that helps businesses to enhance their brand reputation.',
+      checkList: ['It supports Platform Extensibility', 'Gain visibility into critical systems and applications.', 'It allows scheduling & automated Escalations.']
     }
   ];
 
