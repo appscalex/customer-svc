abd51d29e9bf3741e968f89c0e65e56e30434cb0
darkturtle1949 <darkturtle1949@gmail.com>
2026-07-09 10:56:20 +0530

    changes

--- src/app/landing-page1/components/revolution-slider1/revolution-slider1.component.ts
+++ src/app/landing-page1/components/revolution-slider1/revolution-slider1.component.ts
@@ -20,25 +20,59 @@ export class RevolutionSlider1Component implements OnInit, AfterViewInit, OnDest
   ngAfterViewInit() {
     this.ngZone.runOutsideAngular(() => {
       const el = document.getElementById('heroCarousel');
-      if (el && typeof bootstrap !== 'undefined') {
-        this.carousel = new bootstrap.Carousel(el, { interval: 5000, ride: true, pause: false });
+      if (!el) { return; }
+
+      if (typeof bootstrap !== 'undefined') {
+        // Bootstrap 5
+        this.carousel = new bootstrap.Carousel(el, { interval: 4000, ride: true, pause: false });
         el.addEventListener('slid.bs.carousel', (e: any) => {
           this.ngZone.run(() => { this.currentSlide = e.to; });
         });
+      } else {
+        // Bootstrap 4 / jQuery fallback
+        const jq = (window as any).$;
+        if (jq) {
+          jq(el).carousel({ interval: 4000, ride: 'carousel', pause: false });
+          jq(el).on('slid.bs.carousel', (e: any) => {
+            this.ngZone.run(() => { this.currentSlide = e.to; });
+          });
+          this.carousel = 'jquery';
+        }
       }
     });
   }
 
   ngOnDestroy() {
-    if (this.carousel) {
+    if (this.carousel && this.carousel !== 'jquery') {
       this.carousel.dispose();
     }
   }
 
   goToSlide(index: number) {
     this.currentSlide = index;
-    if (this.carousel) {
+    if (this.carousel && this.carousel !== 'jquery') {
       this.carousel.to(index);
+    } else {
+      const jq = (window as any).$;
+      if (jq) { jq('#heroCarousel').carousel(index); }
+    }
+  }
+
+  prevSlide() {
+    if (this.carousel && this.carousel !== 'jquery') {
+      this.carousel.prev();
+    } else {
+      const jq = (window as any).$;
+      if (jq) { jq('#heroCarousel').carousel('prev'); }
+    }
+  }
+
+  nextSlide() {
+    if (this.carousel && this.carousel !== 'jquery') {
+      this.carousel.next();
+    } else {
+      const jq = (window as any).$;
+      if (jq) { jq('#heroCarousel').carousel('next'); }
     }
   }
 }
